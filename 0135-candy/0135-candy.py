0e18ee6d7be5d4d657c694e70ec2ca234b1c5238
subhash2405 <136123516+subhash2405@users.noreply.github.com>
2024-01-12 20:51:01 +0530

    Time: 130 ms (82.20%), Space: 20.2 MB (21.21%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,15 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        arr=[1]*len(ratings)
+        for i in range(len(ratings)-1):
+            if ratings[i]<ratings[i+1]:
+                arr[i+1]=arr[i]+1
+        j=len(ratings)-1
+        while j>0:
+            if ratings[j-1]>ratings[j] and arr[j-1]<=arr[j]:
+                arr[j-1]=arr[j]+1
+            j-=1
+        sum=0
+        for i in range(len(arr)):
+            sum+=arr[i]
+        return sum
