fcf0c62b5f7417420443c9497e9d61233929b5dc
Subhash Chandra Ponnam <136123516+subhash2405@users.noreply.github.com>
2024-06-28 11:39:48 +0530

    Time: 1277 ms (43.03%), Space: 44.6 MB (20.08%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
@@ -1,7 +1,3 @@
-import heapq
-from collections import Counter
-from typing import List
-
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
         arr = []
@@ -9,7 +5,6 @@ class Solution:
             arr.append(roads[i][0])
             arr.append(roads[i][1])
         count = Counter(arr)
-        print(count)
         temp = list(set(arr))
         heap = []
         for i in range(len(temp)):
