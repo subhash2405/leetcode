e9acee32cdd30c5f7a783806ac8018eda90589b4
Subhash Chandra Ponnam <136123516+subhash2405@users.noreply.github.com>
2024-11-02 18:32:22 +0530

    Time: 11 ms (64.83%), Space: 16.8 MB (10.02%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,23 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        self.rec(0,[],candidates, res, target)
-        return res
 
-    
+        def check(temp, add, index):
+            if add == target:
+                res.append(temp)
+                return 
+            elif add>target or index==len(candidates):
+                return 
+            var = temp.copy()
+            var.append(candidates[index])
+            check(var, add+candidates[index], index)
+            check(temp, add, index+1)
 
-    def rec(self, add, subset, candidates, res, target):
-        if add == target:
-            subset.sort()
-            if subset not in res:
-                res.append(subset)
-            return res
-        if (add>target) or (min(candidates)+add>target):
-            return res
-        for i in range(len(candidates)):
-            sub1 = subset.copy()
-            sub1.append(candidates[i])
-            self.rec(add + candidates[i],sub1, candidates, res, target )
+        check([],0,0)
         return res
-        
-            
