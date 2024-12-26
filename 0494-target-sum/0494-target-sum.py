ae70c909fb069da58f1c0ba00db1113796f30d7b
Subhash Chandra Ponnam <136123516+subhash2405@users.noreply.github.com>
2024-12-26 19:39:51 +0530

    Time: 211 ms (15.45%), Space: 38.6 MB (17.20%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -2,22 +2,19 @@ from typing import List
 
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        total_sum = sum(nums)
-        if total_sum < abs(target):
-            return 0
+        memo = {}
+        def dp(index,curr):
+            if index==len(nums):
+                return 1 if curr==target else 0
+
+            if (index,curr) in memo:
+                return memo[(index,curr)]
+
+            add = dp(index+1, curr+nums[index])
+            sub = dp(index+1, curr-nums[index])
+
+            memo[(index, curr)] = add+sub
+
+            return memo[(index,curr)]
         
-        dp = {}
-        return self.oper(nums, dp, len(nums)-1, target)
-    
-    def oper(self, nums, dp, n, target):
-        if n == -1:
-            return 1 if target == 0 else 0
-        
-        if (n, target) in dp:
-            return dp[(n, target)]
-        
-        positive = self.oper(nums, dp, n-1, target-nums[n])
-        negative = self.oper(nums, dp, n-1, target+nums[n])
-        
-        dp[(n, target)] = positive + negative
-        return dp[(n, target)]
+        return dp(0,0)
